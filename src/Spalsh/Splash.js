77fd24a7455568107246a075c6c57dfbbe648ebd
KJS <digyrh456789@gmail.com>
2023-05-23 19:46:37 +0900

    UPDATE :: SPLASH SCREEN 완성

--- src/Spalsh/Splash.js
+++ src/Spalsh/Splash.js
@@ -1,38 +1,75 @@
 import React, { useState } from 'react';
 import { useSpring, animated, useChain, useSpringRef } from "react-spring";
 import styled, { keyframes } from 'styled-components';
-export default function Splash() {
-    const [text, setText] = useState('White and Black');
-    const text1Ref = useSpringRef();
-    const text2Ref = useSpringRef();
-    const text3Ref = useSpringRef();
 
-    const text1Style = useAnimation(text1Ref);
-    const text2Style = useAnimation(text2Ref);
-    const text3Style = useAnimation(text3Ref);
-
-    useChain([text1Ref, text2Ref, text3Ref]);
-    return (
-        <SplashMain>
-            <Texting>
-                <animated.h1 style={text1Style}>{text}</animated.h1>
-            </Texting>
-        </SplashMain>
-    );
-}
-
+const deleteanimation = keyframes`
+	from {
+        opacity: 1;
+    }
+    to {
+        opacity: 0;
+    }
+`;
 const wbanimation = keyframes`
 	from {
-        margin-left: 2.5%;
-        letter-spacing: 50px;
+        margin-left: 5vw;
+        letter-spacing: 60px;
         animation-timing-function:ease-in-out;
     }
     to {
-        margin-left: 18%;
+        margin-left: 18vw;
         letter-spacing: 10px;
         animation-timing-function:ease-in-out;
     }
 `;
+const setposition = keyframes`
+    from {
+    }
+    to{
+        margin-left: 15.5vw;
+    }
+`
+const mining = keyframes`
+    from {
+        margin-left: 8vw;
+    }
+    to {
+        margin-left: -7.5vw;
+    }
+`
+export default function Splash() {
+    const text1Ref = useSpringRef();
+    const text1Style = useAnimation(text1Ref);
+    useChain([text1Ref]);
+    return (
+        <SplashMain>
+            <Texting>
+                <animated.h1 style={text1Style}><Exceptions>W</Exceptions><Disappearing>hite</Disappearing>
+                    <Minusmargin>
+                        & B<Disappearing>lack</Disappearing>
+                    </Minusmargin>
+                </animated.h1>
+            </Texting>
+        </SplashMain>
+    );
+}
+const Minusmargin = styled.span`
+    animation: ${mining} 1s 1s;
+    margin-left: 8vw;
+    animation-timing-function: ease-in-out;
+    animation-fill-mode: forwards;
+`
+const Exceptions = styled.span`
+    animation: ${setposition} 1s 1s;
+    animation-timing-function: ease-in-out;
+    animation-fill-mode: forwards;
+`
+const Disappearing = styled.span`
+    animation: ${deleteanimation} 1s 1s;
+    animation-timing-function:ease-in-out;
+    //애니메이션 끝나더라도 유지하깅
+    animation-fill-mode: forwards;
+`
 
 const useAnimation = ref => {
     const spring = useSpring({
@@ -60,14 +97,15 @@ const SplashMain = styled.div`
 `
 const Texting = styled.span`
     position: absolute;
-    margin-left: 2.5%;
-    letter-spacing: 50px;
-    margin-top: 13%;
-    font-size: 80px;
+    margin-left: 5vw;
+    letter-spacing: 60px;
+    margin-top: 25vh;
+    font-size: 4.1vw;
     font-family: 'Readline';
     color: #fff;
     text-shadow: 0 0 0px #f8f8ff, 0 0 0px #f8f8ff, 0 0 10px #f8f8ff, 0 0 70px #f8f8ff, 0 0 100px #f8f8ff;
     animation: ${wbanimation} 1s 1s;
     animation-timing-function:ease-in-out;
+    //애니메이션 끝나더라도 유지하깅
     animation-fill-mode: forwards;
 `
